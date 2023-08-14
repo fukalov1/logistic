b66b883e35a0a5abccb579e80e51dd401a4e3d50
fai <fai_@mail.ru>
2023-08-14 11:18:59 +0400

    redesing site

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -1,3 +1,7 @@
 <link rel="stylesheet" href="{{asset('/style/style.css')}}">
 <link rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
 <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+<style>
+    input { font-size: 16px; }
+    input:focus{font-size:16px;}
+</style>
